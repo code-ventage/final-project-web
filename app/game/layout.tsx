7f9f14081fe61caa2380ceecb9c1f0415dfae8c9
Marcos Sebastian Portales Ramos <marcosportales2002@gmail.com>
2024-07-03 13:50:33 -0400

    feat: Add game page

---
+++ app/game/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from '@/components/navbar/navbar'
+
+export default function GameLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main className="flex min-h-screen w-full flex-col items-center justify-center">
+        {children}
+      </main>
+    </div>
+  )
+}
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,13 +1,53 @@
-import InstructStepper from '@/components/instruct-stepper'
-import Navbar from '@/components/navbar'
+'use client'
+
+import Game from '@/components/game/game'
+import GameResults from '@/components/game/game-results'
+import Countdown from '@/components/game/countdown'
+import AuthContext from '@/context/auth-context'
+import { useContext, useState } from 'react'
+import Info from '@/components/game/info'
+import Link from 'next/link'
 
 export default function GamePage() {
-  return (
-    <div className="">
-      <Navbar />
-      <main>
-        <InstructStepper />
+  const { user } = useContext(AuthContext)
+  const [gameStatus, setGameStatus] = useState(0)
+  const [userScore, setUserScore] = useState(0)
+
+  const views = [
+    <Info key={'first'} setGameStatus={setGameStatus} />,
+    <Countdown key={'second'} setGameStatus={setGameStatus} />,
+    <Game
+      key={'third'}
+      setGameStatus={setGameStatus}
+      saveUserScore={setUserScore}
+    />,
+    <GameResults
+      key={'fourth'}
+      score={userScore}
+      setGameStatus={setGameStatus}
+    />,
+  ]
+
+  if (!user) {
+    return (
+      <main className="flex flex-col items-center justify-center">
+        <span>
+          Debes{' '}
+          <Link
+            href="/login"
+            className="font-medium text-blue-600 hover:underline"
+          >
+            {' '}
+            iniciar sesión{' '}
+          </Link>{' '}
+          para jugar
+        </span>
       </main>
-    </div>
+    )
+  }
+  return (
+    <main className="flex w-full flex-col items-center md:justify-center">
+      {views[gameStatus]}
+    </main>
   )
 }
